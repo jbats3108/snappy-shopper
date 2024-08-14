2e390c74d5a45ddc006b3c8f55d64048c10edd4c
Jamie Batabyal <jamiebatabyal@gmail.com>
2024-08-14 21:35:23 +0100

    Created postcodes table

---
+++ database/migrations/2024_08_14_203214_create_postcodes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('postcodes', function (Blueprint $table) {
+            $table->id();
+            $table->string('postcode');
+            $table->string('outcode');
+            $table->string('incode');
+            $table->string('longitude');
+            $table->string('latitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('postcodes');
+    }
+};
