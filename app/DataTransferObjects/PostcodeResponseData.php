a292f7ba0c1ff7bcf0744a700b9f72bf00bc0f63
Jamie Batabyal <jamiebatabyal@gmail.com>
2024-08-16 11:13:13 +0100

    Created Postcode Response DTO

---
+++ app/DataTransferObjects/PostcodeResponseData.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataTransferObjects;
+
+use Spatie\LaravelData\Data;
+
+class PostcodeResponseData extends Data
+{
+    public function __construct(
+        public readonly string $postcode,
+        public readonly string $outcode,
+        public readonly string $incode,
+        public readonly string $longitude,
+        public readonly string $latitude
+    ) {
+    }
+
+    public static function fromResponse(array $responseResult): self
+    {
+        return new self(
+            $responseResult['postcode'],
+            $responseResult['outcode'],
+            $responseResult['incode'],
+            (string)$responseResult['longitude'],
+            (string)$responseResult['latitude']
+        );
+    }
+}
