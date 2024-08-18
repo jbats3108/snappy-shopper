a0db8e0514932a0755e5a04bbfb9dec031cbb141
Jamie Batabyal <jamiebatabyal@gmail.com>
2024-08-18 14:39:30 +0100

    Command to import postcodes in bulk via CSV

---
+++ app/Console/Commands/ImportPostcodes.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Postcode;
+use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
+
+class ImportPostcodes extends Command
+{
+    protected $signature = 'app:import-postcodes {filename : Filename of the csv containing the postcodes to import}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    public function handle(): void
+    {
+        $filename = $this->argument('filename');
+
+        $file = collect(file($filename));
+
+        $file->shift();
+
+        $data = $file->map(function (string $row) {
+            $row = str_getcsv($row);
+
+            $postcode = $row[0];
+            $splitPostcode = explode(' ', $postcode);
+            $outcode = $splitPostcode[0];
+            $incode = $splitPostcode[1];
+
+            return
+                [
+                    'postcode' => $postcode,
+                    'outcode' => $outcode,
+                    'incode' => $incode,
+                    'latitude' => $row[1],
+                    'longitude' => $row[2],
+                ];
+        });
+
+        $data->chunk(1000)->each(function (Collection $chunk) {
+            $chunk->each(
+                fn(array $row) => Postcode::upsert($row, ['postcode', 'outcode', 'incode'], ['latitude', 'longitude'])
+            );
+        });
+    }
+}
---
+++ tests/Unit/Console/Commands/ImportPostcodesTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests\Unit\Console\Commands;
+
+use App\Models\Postcode;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class ImportPostcodesTest extends TestCase
+{
+    use WithFaker;
+    use RefreshDatabase;
+
+    #[Test] public function it_imports_a_csv_of_postcodes_and_coordinates_into_the_postcodes_table()
+    {
+        // Given
+        $filename = 'test.csv';
+
+        if (file_exists($filename)) {
+            unlink($filename);
+        }
+
+        $handler = fopen($filename, 'w');
+
+        fputcsv($handler, ['postcode', 'latitude', 'longitude']);
+
+        $data =
+            [
+                ['AB1 1AB', $this->faker->latitude, $this->faker->longitude],
+                ['C2 2CV', $this->faker->latitude, $this->faker->longitude],
+                ['EX1 1XE', $this->faker->latitude, $this->faker->longitude],
+            ];
+
+        foreach ($data as $row) {
+            fputcsv($handler, $row);
+        }
+
+        fclose($handler);
+
+        // When
+        $this->artisan('app:import-postcodes', ['filename' => $filename]);
+
+        // Then
+        $this->assertDatabaseCount('postcodes', 3);
+
+        foreach ($data as $importedRow) {
+            $this->assertDatabaseHas('postcodes', ['postcode' => $importedRow[0]]);
+        }
+
+        unlink($filename);
+    }
+
+    #[Test] public function it_will_update_an_existing_row_if_the_postcode_already_exists()
+    {
+        // Given
+        $existingPostcode = Postcode::factory()->create();
+
+        $filename = 'test.csv';
+
+        if (file_exists($filename)) {
+            unlink($filename);
+        }
+
+        $handler = fopen($filename, 'w');
+
+        fputcsv($handler, ['postcode', 'latitude', 'longitude']);
+
+        $newLatitude = 1234.56;
+        $newLongitude = -0.33344;
+        $data =
+            [
+                [$existingPostcode->postcode, $newLatitude, $newLongitude],
+                ['C2 2CV', $this->faker->latitude, $this->faker->longitude],
+                ['EX1 1XE', $this->faker->latitude, $this->faker->longitude],
+            ];
+
+        foreach ($data as $row) {
+            fputcsv($handler, $row);
+        }
+
+        fclose($handler);
+
+        // When
+        $this->artisan('app:import-postcodes', ['filename' => $filename]);
+
+        // Then
+        $this->assertDatabaseCount('postcodes', 3);
+
+        $existingPostcode->refresh();
+
+        $this->assertEquals($newLatitude, $existingPostcode->latitude);
+        $this->assertEquals($newLongitude, $existingPostcode->longitude);
+
+        unlink($filename);
+    }
+}
